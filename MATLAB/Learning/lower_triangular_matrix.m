e440bb4d42905f47309eb6418d9c721cc363e73a
Luoxiaogan <1601710607@qq.com>
2023-08-24 02:37:47 +0800

    b

--- MATLAB/Learning/lower_triangular_matrix.m
+++ MATLAB/Learning/lower_triangular_matrix.m
@@ -1,2 +1,12 @@
 %Program 1.5 Gaussian Elimination
-%compute approximate solution of 
+%compute approximate solution of AX=b, where A is a lower triangular matrix
+%Inpute: matrix L, colume b
+%Output: solution X
+function xc=lower_triangular_matrix(L,b)
+    n=length(L); %a lower triangular matrix A is absolutly n*n
+    for j=1:n-1
+        b(j)=b(j)/L(j,j);
+        b(j+1:n)=b(j+1:n)-b(j+1:n)-b(j)*L(j+1:n,j);
+    end
+    b(n)=b(n)/L(n,n);
+    xc=b;
